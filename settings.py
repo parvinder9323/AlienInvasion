c1790f7733c43590f65ad33d82724d97409c0287
parvinder9323 <singh.parvinder30@outlook.com>
2023-05-02 14:01:19 +0530

    Speed settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,11 @@
 class Settings:
     """A class to store all settings for Alien Invasion."""
-    def __int__(self):
+    def __init__(self):
         """Initialize the game's settings."""
         # Screen Settings
         self.screen_width = 1200
         self.screen_height = 600
         self.bg_color = (240, 240, 240)
+
+        # Ship Settings
+        self.ship_speed = 3.5
